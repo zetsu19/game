5022e384443c619e8c3c02ac7130825bc7cb45c5
25HP0465 25HP0465 <25HP0465@G301-CC0203-2.local>
2025-07-01 17:17:10 +0800

    first commit

--- game.js
+++ game.js
@@ -1,16 +1,72 @@
 const gameContainer = document.getElementById("game");
+const startButton = document.getElementById("start");
+const restartButton = document.getElementById("restart");
+let previousHoleId = null;
+let scores = 0;
+let hugtsa = 10;
+let scoreInterval = null;
+let timeInterval = null;
 
 function togloom() {
   for (let i = 0; i < 9; i++) {
     const hole = document.createElement("div");
     hole.classList.add("hole");
     hole.id = i.toString();
-    gameContainer.appendChild(hole);
     const mole = document.createElement("div");
     mole.classList.add("moles");
-    gameContainer.appendChild(hole);
+    mole.style.display = "none";
+    mole.addEventListener("click", function () {
+      console.log("working");
+      const count = document.getElementById("playerScore");
+      count.innerText = Number(count.innerText) + 1;
+    });
     hole.appendChild(mole);
+    gameContainer.appendChild(hole);
   }
 }
+
+function starter() {
+  hide();
+  const randomNumber = Math.floor(Math.random() * 9);
+  console.log(randomNumber, "THIS HOLE");
+
+  const randomHole = document.getElementById(randomNumber);
+  console.log(randomHole);
+
+  const randomMole = randomHole.querySelector(".moles");
+  console.log(randomMole);
+
+  randomMole.style.display = "block";
+
+  previousHoleId = randomNumber;
+}
+function time() {
+  hugtsa = hugtsa - 1;
+  const timer = document.getElementById("playerTime");
+  timer.innerHTML = `${hugtsa} sec`;
+  if (hugtsa <= 0) {
+    clearInterval(scoreInterval);
+    clearInterval(timeInterval);
+    const lastmole = document.getElementById(previousHoleId);
+    lastmole.querySelector(".moles").style.display = "none";
+    const count = document.getElementById("playerScore");
+    alert(`${count}`);
+  }
+}
+function RESTART() {}
+restartButton.addEventListener("click", RESTART);
+
+function hide() {
+  if (previousHoleId !== null) {
+    const umnuhNuh = document.getElementById(previousHoleId);
+    const umnuhSohorNomin = umnuhNuh.querySelector(".moles");
+    umnuhSohorNomin.style.display = "none";
+  }
+}
+
+startButton.addEventListener("click", function () {
+  scoreInterval = setInterval(starter, 1000);
+  timeInterval = setInterval(time, 500);
+});
 
 window.onload = togloom;
